29572ef3b34fd2a01d9675117a9b197a7f25fd72
sharvari-09 <jadhavsharvari9113@gmail.com>
2025-07-14 07:32:00 +0530

    Response Generated

--- src/main/java/com/email/email_res_gen/app/EmailGeneratorController.java
+++ src/main/java/com/email/email_res_gen/app/EmailGeneratorController.java
@@ -2,13 +2,11 @@ package com.email.email_res_gen.app;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private EmailGeneratorService emailGeneratorService;
